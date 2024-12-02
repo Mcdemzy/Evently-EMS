41bb5c05cc8652b745e939ff08eb59c979a407e6
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-12-02 19:35:52 +0100

    "Updated ContactPage and TicketsPage components with dark mode styling and minor layout changes."

--- src/components/Tickets/TicketsPage.tsx
+++ src/components/Tickets/TicketsPage.tsx
@@ -55,88 +55,93 @@ const TicketsPage = () => {
   };
 
   return (
-    <div className="w-full max-w-7xl mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-4">Checkout</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        {/* Left Section */}
-        <div className="md:col-span-2">
-          <h2 className="text-lg font-semibold mb-4">Choose Tickets</h2>
-          {Object.keys(ticketDetails).map((key) => (
-            <div
-              key={key}
-              className="flex items-center justify-between border-b py-4"
-            >
-              <div>
-                <h3 className="font-bold">{key.toUpperCase()}</h3>
-                <p className="text-sm text-gray-600">
-                  {`₦${ticketDetails[
-                    key as TicketType
-                  ].price.toLocaleString()} includes ₦${ticketDetails[
-                    key as TicketType
-                  ].fee.toLocaleString()} fee`}
-                </p>
-                <p className="text-sm">
-                  {ticketDetails[key as TicketType].description}
-                </p>
-              </div>
-              <select
-                className="border rounded-md p-2"
-                value={tickets[key as TicketType]}
-                onChange={(e) =>
-                  handleChange(key as TicketType, Number(e.target.value))
-                }
+    <main className="min-h-screen dark:bg-[#000000] dark:text-white">
+      <div className="w-full max-w-7xl mx-auto p-6">
+        <h1 className="text-2xl font-bold mb-4">Checkout</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          {/* Left Section */}
+          <div className="md:col-span-2">
+            <h2 className="text-lg font-semibold mb-4">Choose Tickets</h2>
+            {Object.keys(ticketDetails).map((key) => (
+              <div
+                key={key}
+                className="flex items-center justify-between border-b py-4 gap-[100px] "
               >
-                {[...Array(11)].map((_, index) => (
-                  <option key={index} value={index}>
-                    {index}
-                  </option>
-                ))}
-              </select>
-            </div>
-          ))}
-        </div>
-
-        {/* Right Section */}
-        <div className="border p-4 rounded-lg shadow-lg">
-          <h2 className="text-lg font-semibold mb-4">Summary</h2>
-          <div className="mb-6">
-            <h3 className="font-bold">PARTY W ZEIGHT</h3>
-            {totalPrice === 0 ? (
-              <p className="text-sm text-gray-600">
-                Please choose a ticket type to continue
-              </p>
-            ) : (
-              <ul className="text-sm space-y-2">
-                {Object.entries(tickets)
-                  .filter(([_, quantity]) => quantity > 0)
-                  .map(([key, quantity]) => (
-                    <li key={key}>
-                      {quantity} x {key.toUpperCase()} = ₦
-                      {(
-                        ticketDetails[key as TicketType].price * quantity
-                      ).toLocaleString()}
-                    </li>
+                <div>
+                  <h3 className="font-bold">{key.toUpperCase()}</h3>
+                  <p className="text-sm text-gray-600 dark:text-[#656565]">
+                    <span className="dark:text-[#F24F41]">
+                      {`₦${ticketDetails[
+                        key as TicketType
+                      ].price.toLocaleString()}`}
+                    </span>
+                    {` includes ₦${ticketDetails[
+                      key as TicketType
+                    ].fee.toLocaleString()} fee`}
+                  </p>
+                  <p className="text-sm dark:text-[#656565]">
+                    {ticketDetails[key as TicketType].description}
+                  </p>
+                </div>
+                <select
+                  className="border rounded-md p-2 dark:text-[#3D3D3D]"
+                  value={tickets[key as TicketType]}
+                  onChange={(e) =>
+                    handleChange(key as TicketType, Number(e.target.value))
+                  }
+                >
+                  {[...Array(11)].map((_, index) => (
+                    <option key={index} value={index}>
+                      {index}
+                    </option>
                   ))}
-              </ul>
-            )}
+                </select>
+              </div>
+            ))}
           </div>
-          <div className="flex justify-between items-center font-bold">
-            <span>Total:</span>
-            <span>₦{totalPrice.toLocaleString()}</span>
+
+          {/* Right Section */}
+          <div className="border p-4 rounded-lg shadow-lg">
+            <h2 className="text-lg font-semibold mb-4">Summary</h2>
+            <div className="mb-6">
+              <h3 className="font-bold">PARTY W ZEIGHT</h3>
+              {totalPrice === 0 ? (
+                <p className="text-sm text-gray-600">
+                  Please choose a ticket type to continue
+                </p>
+              ) : (
+                <ul className="text-sm space-y-2">
+                  {Object.entries(tickets)
+                    .filter(([_, quantity]) => quantity > 0)
+                    .map(([key, quantity]) => (
+                      <li key={key}>
+                        {quantity} x {key.toUpperCase()} = ₦
+                        {(
+                          ticketDetails[key as TicketType].price * quantity
+                        ).toLocaleString()}
+                      </li>
+                    ))}
+                </ul>
+              )}
+            </div>
+            <div className="flex justify-between items-center font-bold">
+              <span>Total:</span>
+              <span>₦{totalPrice.toLocaleString()}</span>
+            </div>
+            <button
+              disabled={totalPrice === 0}
+              className={`mt-4 w-full py-2 px-4 rounded-lg ${
+                totalPrice > 0
+                  ? "bg-purple-600 text-white hover:bg-purple-800"
+                  : "bg-gray-300 text-gray-600 cursor-not-allowed"
+              }`}
+            >
+              Proceed to Contact
+            </button>
           </div>
-          <button
-            disabled={totalPrice === 0}
-            className={`mt-4 w-full py-2 px-4 rounded-lg ${
-              totalPrice > 0
-                ? "bg-purple-600 text-white hover:bg-purple-800"
-                : "bg-gray-300 text-gray-600 cursor-not-allowed"
-            }`}
-          >
-            Proceed to Contact
-          </button>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
