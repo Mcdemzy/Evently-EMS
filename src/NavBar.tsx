430e2cd8e0ea01becac5f847f25c0cec90faaaca
Zion <omojolazion@gmail.com>
2024-12-30 15:37:33 +0100

    Initial commit

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,202 +1,157 @@
-import { HiX, HiOutlineMenuAlt3 } from 'react-icons/hi';
-import { IoMdMoon, IoMdSunny } from 'react-icons/io';
-import { NavLink, Link } from 'react-router-dom';
-import { useState } from 'react';
-import { NavBarProps } from './types';
-import useOutsideClick from './components/shared/OutsideClick/useOutsideClick';
+import { HiX, HiOutlineMenuAlt3 } from "react-icons/hi";
+import { IoMdMoon, IoMdSunny } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { NavBarProps } from "./types";
+import AuthButtons from "./components/shared/AuthButtons";
+import NavLinks from "./components/shared/NavLinks";
+import ProfileMenu from "./components/shared/ProfileMenu";
+import useOutsideClick from "./components/shared/OutsideClick/useOutsideClick";
 
 function NavBar({ isDarkMode, toggleDarkMode }: NavBarProps) {
+  const [hasShadow, setHasShadow] = useState(false);
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  const sidebarRef = useOutsideClick(() => setMobileMenuOpen(false));
-  // heyyyy
+  const sidebarRef = useOutsideClick(() => {
+    setMobileMenuOpen(false);
+  });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasShadow(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div
-      className={`navbar sticky top-0 w-full z-10 flex items-center justify-between md:p-10 p-7 bg-slate-50 ${
-        isDarkMode
-          ? 'dark:bg-black text-white shadow-slate-800 shadow-md'
-          : 'light'
-      } bg-opacity-95 shadow-lg`}
-    >
-      <Link to="/" className="flex items-center">
-        <img src="/images/logo.svg" width={30} height={50} alt="Evently logo" />
-        <span className="ml-2 text-3xl font-bold">Evently</span>
-      </Link>
-
-      <div className="space-x-10 lg:block hidden">
-        <NavLink
-          to="/events/all"
-          className={({ isActive }) =>
-            isActive
-              ? 'text-[#FA776C] font-semibold'
-              : 'text-lg font-normal transition-all duration-100 hover:text-[#FA776C]'
-          }
-        >
-          Events
-        </NavLink>
-        <NavLink
-          to="/pricing"
-          className={({ isActive }) =>
-            isActive
-              ? 'text-[#FA776C] font-semibold'
-              : 'text-lg font-normal transition-all duration-100 hover:text-[#FA776C]'
-          }
-        >
-          Pricing
-        </NavLink>
-        <NavLink
-          to="/about"
-          className={({ isActive }) =>
-            isActive
-              ? 'text-[#FA776C] font-semibold'
-              : 'text-lg font-normal transition-all duration-100 hover:text-[#FA776C]'
-          }
-        >
-          About
-        </NavLink>
-        <NavLink
-          to="/contact"
-          className={({ isActive }) =>
-            isActive
-              ? 'text-[#FA776C] font-semibold'
-              : 'text-lg font-normal transition-all duration-100 hover:text-[#FA776C]'
-          }
-        >
-          Contact
-        </NavLink>
-        <NavLink
-          to="/blog"
-          className={({ isActive }) =>
-            isActive
-              ? 'text-[#FA776C] font-semibold'
-              : 'text-lg font-normal transition-all duration-100 hover:text-[#FA776C]'
-          }
-        >
-          My Profile
-        </NavLink>
-      </div>
-
-      <div className="flex items-center gap-6">
-        <div className="gap-x-6 lg:flex hidden items-center">
-          <Link to="/login">
-            <button className="px-5 py-2 rounded-lg hover:text-[#FA776C]">
-              Login
-            </button>
-          </Link>
-          <button className="px-5 py-2 rounded-lg bg-[#624CF5] hover:bg-[#4f46e5] text-white">
-            Sign Up
-          </button>
-        </div>
-        <div
-          onClick={toggleDarkMode}
-          className="relative w-9 h-9 bg-slate-200 dark:bg-black rounded-full p-1 transition-all duration-500 flex items-center justify-between overflow-hidden cursor-pointer"
-        >
-          <button
-            className={`w-full h-auto flex items-center justify-center rounded-full transition-all duration-500 transform ${
-              isDarkMode ? 'translate-x-[-150%]' : 'translate-x-[30%]'
-            }`}
-          >
-            <IoMdMoon size={18} className="text-[#0D0C00]" />
-          </button>
-
-          <button
-            className={`w-full h-auto flex items-center justify-center rounded-full transition-all duration-500 transform ${
-              isDarkMode ? 'translate-x-[-70%]' : 'translate-x-[1500%]'
-            }`}
-          >
-            <IoMdSunny size={18} className="text-[#4F81EC]" />
-          </button>
-        </div>
-        <div className="block lg:hidden z-30">
-          <button
-            onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
-            className="text-2xl focus:outline-none"
-          >
-            {isMobileMenuOpen ? <HiX /> : <HiOutlineMenuAlt3 />}
-          </button>
-        </div>
-      </div>
-
+    <>
       <div
-        ref={sidebarRef}
-        className={`fixed top-0 right-0 h-full w-2/3 max-w-xs bg-slate-50 bg-opacity-95 dark:bg-black dark:bg-opacity-95 text-center flex flex-col items-center pt-20 space-y-6 text-lg border-l border-slate-800 transition-transform transform ${
-          isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'
-        } lg:hidden z-20`}
+        className={`navbar fixed top-0 w-full z-10 flex items-center justify-between md:p-10 p-7
+          ${
+            hasShadow
+              ? "backdrop-blur-lg backdrop-brightness-90 border-b border-white/20 shadow-md"
+              : ""
+          }
+          ${isDarkMode ? "bg-black/40 text-white" : "bg-white/40 text-black"}`}
       >
-        <NavLink
-          to="/events/all"
-          className={({ isActive }) =>
-            isActive
-              ? 'text-[#FA776C] font-semibold'
-              : 'hover:text-[#FA776C] transition duration-200 '
-          }
-          onClick={() => setMobileMenuOpen(false)}
-        >
-          Events
-        </NavLink>
-        <NavLink
-          to="/pricing"
-          className={({ isActive }) =>
-            isActive
-              ? 'text-[#FA776C] font-semibold'
-              : 'hover:text-[#FA776C] transition duration-200'
-          }
-          onClick={() => setMobileMenuOpen(false)}
-        >
-          Pricing
-        </NavLink>
-        <NavLink
-          to="/about"
-          className={({ isActive }) =>
-            isActive
-              ? 'text-[#FA776C] font-semibold'
-              : 'hover:text-[#FA776C] transition duration-200'
-          }
-          onClick={() => setMobileMenuOpen(false)}
-        >
-          About
-        </NavLink>
-        <NavLink
-          to="/contact"
-          className={({ isActive }) =>
-            isActive
-              ? 'text-[#FA776C] font-semibold'
-              : 'hover:text-[#FA776C] transition duration-200'
-          }
-          onClick={() => setMobileMenuOpen(false)}
-        >
-          Contact
-        </NavLink>
-        <NavLink
-          to="/blog"
-          className={({ isActive }) =>
-            isActive
-              ? 'text-[#FA776C] font-semibold'
-              : 'hover:text-[#FA776C] transition duration-200'
-          }
-          onClick={() => setMobileMenuOpen(false)}
-        >
-          My Profile
-        </NavLink>
-        <div className="mt-auto mb-10 flex gap-4 pt-4 border-t dark:border-[#3D3D3D] border-gray-300">
-          <Link to="/login">
-            <button
-              className="px-4 py-2 rounded-lg bg-gray-400 hover:bg-gray-500 transition text-white dark:text-"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              Login
-            </button>
-          </Link>
-          <button
-            className="px-4 py-2 rounded-lg bg-[#624CF5] hover:bg-[#4f46e5] text-white transition"
-            onClick={() => setMobileMenuOpen(false)}
+        <Link to="/" className="flex items-center">
+          <img
+            src="/images/logo.svg"
+            width={30}
+            height={50}
+            alt="Evently logo"
+          />
+          <span className="ml-2 text-3xl font-bold">Evently</span>
+        </Link>
+
+        <NavLinks
+          links={[
+            { path: "/events/all", label: "Events" },
+            { path: "/pricing", label: "Pricing" },
+            { path: "/about", label: "About" },
+            { path: "/contact", label: "Contact" },
+            { path: "/profile", label: "My Profile" },
+          ]}
+          orientation="horizontal"
+        />
+
+        <div className="flex items-center gap-6">
+          <ProfileMenu />
+
+          <AuthButtons isBigScreen={true} />
+
+            {/* Light and dark modes */}
+          <div
+            onClick={toggleDarkMode}
+            className="relative hidden w-9 h-9 bg-slate-200 dark:bg-black rounded-full p-1 transition-all duration-500 lg:flex items-center justify-between overflow-hidden cursor-pointer"
           >
-            Sign Up
-          </button>
+            <button
+              className={`w-full h-auto flex items-center justify-center rounded-full transition-all duration-500 transform ${
+                isDarkMode ? "translate-x-[-150%]" : "translate-x-[30%]"
+              }`}
+            >
+              <IoMdMoon size={18} className="text-[#0D0C00]" />
+            </button>
+
+            <button
+              className={`w-full h-auto flex items-center justify-center rounded-full transition-all duration-500 transform ${
+                isDarkMode ? "translate-x-[-70%]" : "translate-x-[1500%]"
+              }`}
+            >
+              <IoMdSunny size={18} className="text-[#4F81EC]" />
+            </button>
+          </div>
+
+          <div className="block lg:hidden ">
+            <button
+              onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+              className="text-2xl focus:outline-none"
+            >
+              {isMobileMenuOpen ? <HiX /> : <HiOutlineMenuAlt3 />}
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      {isMobileMenuOpen && (
+        <div
+          ref={sidebarRef}
+          className={`
+            ${isMobileMenuOpen ? "translate-x-0" : "translate-x-full"}
+            fixed lg:hidden top-0 right-0 h-full w-2/3 max-w-xs dark:text-white text-center flex flex-col items-center pt-20 space-y-6 text-lg border-l transform transition-transform duration-300 z-[9999] backdrop-blur-lg border-white/20 shadow-md dark:bg-black/50 bg-white/50
+      `}
+          style={{
+            backdropFilter: "blur(10px)",
+          }}
+        >
+          <div className="flex items-center justify-between w-full absolute top-10 right-0 px-7 pr-10 ">
+            <div
+              onClick={toggleDarkMode}
+              className="relative lg:hidden w-9 h-9 bg-slate-200 dark:bg-black rounded-full p-1 transition-all duration-500 flex items-center justify-between overflow-hidden cursor-pointer border border-gray-50"
+            >
+              <button
+                className={`w-full h-auto flex items-center justify-center rounded-full transition-all duration-500 transform ${
+                  isDarkMode ? "translate-x-[-150%]" : "translate-x-[30%]"
+                }`}
+              >
+                <IoMdMoon size={18} className="text-[#0D0C00]" />
+              </button>
+
+              <button
+                className={`w-full h-auto flex items-center justify-center rounded-full transition-all duration-500 transform ${
+                  isDarkMode ? "translate-x-[-70%]" : "translate-x-[1500%]"
+                }`}
+              >
+                <IoMdSunny size={18} className="text-[#4F81EC]" />
+              </button>
+            </div>
+
+            <button
+              onClick={() => setMobileMenuOpen(false)}
+              className="text-3xl focus:outline-none dark:text-white"
+            >
+              <HiX />
+            </button>
+          </div>
+
+          <NavLinks
+            links={[
+              { path: "/events/all", label: "Events" },
+              { path: "/pricing", label: "Pricing" },
+              { path: "/about", label: "About" },
+              { path: "/contact", label: "Contact" },
+              { path: "/blog", label: "My Profile" },
+            ]}
+            orientation="vertical"
+            onClick={() => setMobileMenuOpen(false)}
+          />
+
+          <AuthButtons isBigScreen={false} />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -31,3 +31,24 @@ export interface NavBarProps {
   isDarkMode: boolean;
   toggleDarkMode: () => void;
 }
+
+export interface UserProfileModalProps {
+  isOpen: boolean; 
+  onClose: () => void; 
+}
+
+export interface AuthButtonsProps {
+  onCloseMenu?: () => void;
+  containerClass?: string;
+  isBigScreen?: boolean;
+}
+
+interface NavItem {
+  path: string;
+  label: string;
+}
+export interface NavLinksProps {
+    links: NavItem[];
+    orientation: "horizontal" | "vertical";
+    onClick?: () => void;
+}
