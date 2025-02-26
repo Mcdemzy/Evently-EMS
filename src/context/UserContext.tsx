d60ff576cc31c842495f24e7a0c890abfae51de3
Adeyemo Ademola <adeyemoademola12@gmail.com>
2025-02-26 16:46:36 +0100

    `feat: Added UserContext and updated components to use it`

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,34 @@
+// src/context/UserContext.tsx
+import React, { createContext, useContext, useState } from "react";
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+}
+
+interface UserContextType {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+const UserContext = createContext<UserContextType>({
+  user: null,
+  setUser: () => {},
+});
+
+export const UserProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
