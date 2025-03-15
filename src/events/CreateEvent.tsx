9b4053e391a57a1059271db3e70efd63ec82266d
Zion <omojolazion@gmail.com>
2025-03-15 15:15:41 +0100

    Done With Error 404 Page

--- src/events/CreateEvent.tsx
+++ src/events/CreateEvent.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import Footer from "../components/shared/Footer";
+// import Footer from "../components/shared/Footer";
 export default function CreateEvent() {
   const [physicalEvent, setPhysicalEvent] = useState(false);
   const [onlineEvent, setOnlineEvent] = useState(false);
