fd9b0c3a14215b7a089ffc39410f9567a2a381b7
Zion <omojolazion@gmail.com>
2025-03-16 06:04:12 +0100

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,16 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Increase limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "vendor-react";
+            if (id.includes("chart.js")) return "vendor-chart";
+            return "vendor"; // General vendor chunk
+          }
+        },
+      },
+    },
   },
 });
