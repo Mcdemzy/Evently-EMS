a6516094e1165a3397857cacff539802c2cd91f8
Zion <omojolazion@gmail.com>
2025-03-16 13:30:46 +0100

    Initial Commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
+   build: {
     chunkSizeWarningLimit: 1000, // Increase limit
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "vendor-react";
-            if (id.includes("chart.js")) return "vendor-chart";
-            return "vendor"; // General vendor chunk
-          }
-        },
-      },
-    },
   },
-});
+})
