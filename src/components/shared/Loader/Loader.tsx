ee18e5fc6d3836f5993cdba6db37946a0b16c4aa
Adeyemo Ademola <adeyemoademola12@gmail.com>
2025-03-19 22:39:17 +0100

    `Update Loader component styles to use new color palette`

--- src/components/shared/Loader/Loader.tsx
+++ src/components/shared/Loader/Loader.tsx
@@ -7,7 +7,7 @@ const Loader: React.FC = () => {
     <div className="fixed top-0 left-0 w-full h-full backdrop-blur-md border-white/20 shadow-md bg-black/5 flex items-center justify-center z-50">
       <div className="loader">
         <div className="box">
-          <div className="logo">
+          <div className="logo rounded-full bg-white">
             <img src={logo} alt="Logo" className="logo-svg" />
           </div>
         </div>
